8969e51281c6bd67f46f36d4992f57d927929e6d
sumitpgupta <sumitpgupta@gmail.com>
2017-12-30 17:46:11 -0500

    Commit 2

---
+++ Churn.py
@@ -0,0 +1,162 @@
+# Part 1: Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Churn_Modelling.csv')
+x = dataset.iloc[:,3:13].values
+y = dataset.iloc[:,13].values
+
+# Encoding categorical Data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x_1 = LabelEncoder()
+x[:,1]= labelencoder_x_1.fit_transform(x[:, 1])
+
+labelencoder_x_2 = LabelEncoder()
+x[:,2] = labelencoder_x_2.fit_transform(x[:,2])
+
+onehotencoder = OneHotEncoder(categorical_features=[1])
+x = onehotencoder.fit_transform(x).toarray()
+x = x[:,1:]
+
+
+# Splitting the dataset into train and test set 
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=0)
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test  = sc.fit_transform(x_test)
+
+## Part II: Making ANN
+
+# Importing the Keras package
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+
+# Initializing the ANN
+classifier = Sequential()
+
+# Adding the input layer and first hidden layer
+classifier.add(Dense(output_dim = 6, init = 'uniform',activation='relu', input_dim = 11))
+# With dropout
+# classifier.add(Dropout(p=0.1))
+
+ 
+# Adding the second hidden layer
+classifier.add(Dense(output_dim = 6, init = 'uniform',activation='relu'))
+# With dropout
+# classifier.add(Dropout(p=0.1))
+
+# Adding the output layer
+classifier.add(Dense(output_dim = 1, init = 'uniform',activation='sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Fitting ANN to train set
+classifier.fit(x_train, y_train, batch_size=10, nb_epoch = 100)
+
+## Making Predictions and Evaluating the Model
+
+# Predicting the test set results:
+y_pred = classifier.predict(x_test)
+y_pred = (y_pred > 0.5)
+
+# Building the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+cm
+
+# Checking the Accuracy
+"""Accuracy = (1538+142)/(1538+57+263+142)
+Accuracy"""
+    
+##### New test case #####
+
+""" Predict if the following customre will churn/leave the bank:
+Geography : France
+Credit Score : 600
+Gender: Male
+Age : 40
+Tenure: 3
+Balance: 60000
+Number of Products: 2
+Has Credit Card: Yes
+Is Active member: Yes
+Estimated Salary : 50000 """
+
+new_pred = classifier.predict(sc.transform([[0,0,600,1,40,3,60000,2,1,1,50000]]))
+new_pred = (new_pred>0.5)
+new_pred
+
+# Further refining the evaluation using K-fold cross-validation.
+""" Since, K -fold cross validation is in scikit learn we will have to 
+establish a relation between both for which we use below function"""
+
+from keras.wrappers.scikit_learn import KerasClassifier 
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.layers import Dense
+
+"""def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(output_dim = 6, init = 'uniform',activation='relu', input_dim = 11))
+    classifier.add(Dense(output_dim = 6, init = 'uniform',activation='relu'))
+    classifier.add(Dense(output_dim = 1, init = 'uniform',activation='sigmoid'))
+    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+
+
+classifier = KerasClassifier(build_fn= build_classifier, batch_size = 10, nb_epoch = 100)
+# Computing accuracies of 10 folds
+accuracies = cross_val_score(estimator=classifier, X = x_train, y= y_train, cv=3, n_jobs= -1) 
+
+# mean = accuracies.mean() # Computing means of accuracies of 10 folds
+# variance = accuracies.std() """
+
+# Dropout regulrization to reduce overfitting if required
+
+
+# Tuning the ANN
+from keras.wrappers.scikit_learn import KerasClassifier 
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(output_dim = 6, init = 'uniform',activation='relu', input_dim = 11))
+    classifier.add(Dense(output_dim = 6, init = 'uniform',activation='relu'))
+    classifier.add(Dense(output_dim = 1, init = 'uniform',activation='sigmoid'))
+    classifier.compile(optimizer= optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn=build_classifier)
+parameters = {'batch_size': [25,32],
+              'nb_epoch': [100,500],
+              'optimizer':['adam','rmsprop']}
+grid_search = GridSearchCV(estimator=classifier, 
+                           param_grid=parameters,
+                           scoring='accuracy',
+                           cv=10)
+grid_search = grid_search.fit(x_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
+
+""" Best parameters are:
+    Batch_size: 25
+    nb_epoch : 100
+    optimizer: 'adam' 
+    
+    Best Accuracy = 82.88% """
+    
+    
+
+
